687aed02c0d2656b01c18d5a276e72bc9692e6e3
Salih Erdoğan <salihsel@gmail.com>
2017-08-08 16:43:40 +0300

    Update and rename a.js to b.js

--- b.js
+++ b.js
@@ -9,23 +9,12 @@ next();
 //omx.on('stop', function(){next();});
 
 function next(){
-    const args = [
-        "-t",
-        "-f",
-        "-i",
-        "LAST_FRAME.PNG"
-    ];
-    
+
     mu=spawn('omxplayer', [listem[index]]);
     mu.on('close', (code) => {
         mu.kill();
         next();
     });
-    vu=spawn('pqiv', args);
-    vu.on('close', (code) => {
-        vu.kill();
-    });
-    // omx.play(listem[index]);
     index++;
     index=index%3;
  
