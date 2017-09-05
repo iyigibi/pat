ad5bcf47879eb32436ee47449dcaef89e9620114
Salih Erdoğan <salihsel@gmail.com>
2017-09-05 11:41:28 +0300

    Update b.js

--- b.js
+++ b.js
@@ -5,7 +5,7 @@ var index=0;
 var omx = require('omxdirector');
 var mu;
 var vu;
-next();
+//next();
 omx.on('stop', function(){next();});
 
 function next(){
@@ -18,3 +18,13 @@ function next(){
 }
 //ffmpeg -i a3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts a3.ts
 //ffmpeg -i "concat:a1.ts|a2.ts" -c copy -bsf:a aac_adtstoasc wer.mp4
+
+
+var soap = require('soap');
+  var url = 'http://yayin.medya.istanbul/broadcast/wssignage.asmx';
+  var args = {name: 'value'};
+  soap.createClient(url, function(err, client) {
+      client.GetDate(args, function(err, result) {
+          console.log(result);
+      });
+  });
