f6f5c96527211fa56e95dff97b772fe385bdf675
Salih Erdoğan <salihsel@gmail.com>
2017-09-07 09:48:57 +0300

    Update b.js

--- b.js
+++ b.js
@@ -102,7 +102,7 @@ function loginIt(){
 					//console.log("buu");
 					//console.log("http://yayin.medya.istanbul/broadcast/upload/"+sstr.substring(0,sstr.lastIndexOf("."))+"_720p.mp4");
 					var url__=sstr.substring(0,sstr.lastIndexOf("."));
-					var fname_=url__.substring(url__.lastIndexOf("/"),-1)+"_720p.mp4";
+					var fname_=url__.substring(url__.lastIndexOf("/"),10)+"_720p.mp4";
 					console.log("buu "+url__);
 					console.log("buu "+fname_);
 					var url = "http://yayin.medya.istanbul/broadcast/upload/"+url__+"_720p.mp4";
