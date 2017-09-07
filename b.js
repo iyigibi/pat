595ab83e73694afeb298504b9b31bc0b1f543a9b
Salih Erdoğan <salihsel@gmail.com>
2017-09-07 13:42:41 +0300

    Update b.js

--- b.js
+++ b.js
@@ -118,7 +118,7 @@ function loginIt(){
 						}
 						download(url, options, function(err){
 						    if (err) throw err
-						    console.log("meow "+url);
+						    console.log("meow "+this.url);
 										var args_ = ['-i', options.directory+"/"+options.filename, '-f',
 											     'mpegts',options.filename+".ts"];
 										var ffmpeg = spawn('ffmpeg', args_);
