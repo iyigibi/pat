d09840be9235e16b77f8451b3fffbec3c814181e
Salih Erdoğan <salihsel@gmail.com>
2017-09-07 10:08:02 +0300

    Update b.js

--- b.js
+++ b.js
@@ -104,7 +104,7 @@ function loginIt(){
 					var url__=sstr.substring(0,sstr.lastIndexOf("."));
 					
 					var url = "http://yayin.medya.istanbul/broadcast/upload/"+url__+"_720p.mp4";
-					var tt_=url.replace(/\\/g, '#')
+					var tt_=url.replace(/\\/g, '')
 					var int_=tt_.lastIndexOf("#");
 					var fname_=url.substring(int_,url.length-int_);
 					console.log("buu "+fname_+" "+tt_);
